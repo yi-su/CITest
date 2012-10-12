46e2f70636d29ba55ca718acc47c6427566af73f
Yi Su <yi.su@logicube.com.au>
2012-10-13 00:55:19 +0100

    Init Import

---
+++ UnitTest/ValueControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CITest.Controllers;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class ValueControllerTest
+    {
+        [Test]
+        public void Get_Success()
+        {
+            var controller = new ValuesController();
+            var result = controller.Get().ToArray();
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result[0]);
+            Assert.AreEqual("value2", result[1]);
+        }
+    }
+}
